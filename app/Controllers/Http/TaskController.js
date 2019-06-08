3d6e358bab21a488a4294b25208e08e947f46a22
Andres <andresdamelio24@gmail.com>
2019-06-08 16:46:48 -0400

    CRUD

--- app/Controllers/Http/TaskController.js
+++ app/Controllers/Http/TaskController.js
@@ -7,19 +7,55 @@ class TaskController {
         let tasks = await Task.all()
         return response.json(tasks)
     }
+
     async store ({ request, response }) {
         const taskInfo = request.only(['task_name'])
         const task = new Task()
         task.task_name = taskInfo.task_name
 
         await task.save()
-        return response.status(200).json(task )
+        return response.status(200).json(task)
     }
+
     async show ({ params, response }) {
         let task = await Task.find(params.id)
         return response.json(task)
 
     }
+
+    async update ({ params, request, response }) {
+        const taskInfo = request.only(['task_name'])
+        const task = await Task.find(params.id)
+
+        if(!task){
+
+            return response.status(404).json({data: 'Tarea no encontrada'})
+
+        }
+        
+        task.task_name = taskInfo.task_name
+
+        await task.save()
+
+        return response.status(200).json(task)
+
+    }
+
+    async delete ({ params, response }) {
+        let task = await Task.find(params.id)
+        
+        if(!task){
+            return response.status(404).json({data: 'Tarea no encontrada'})
+        }
+
+        await task.delete()
+
+        return response.status(200).json(task)
+
+
+    }
+
+
 }
 
 module.exports = TaskController
--- start/routes.js
+++ start/routes.js
@@ -22,6 +22,6 @@ Route.group(()=> {
     Route.get('/tasks','TaskController.index')
     Route.get('/task/:id','TaskController.show')
     Route.post('/task','TaskController.store')
-    Route.put('/tasks','TaskController.update')
-    Route.delete('/tasks','TaskController.delete')
+    Route.put('/task/:id','TaskController.update')
+    Route.delete('/task/:id','TaskController.delete')
 }).prefix('api');
